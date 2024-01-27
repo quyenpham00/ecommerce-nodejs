3e1c63caa0013febd32c7e0ad695c1b588399faf
Pham Thi My Quyen <quyen.ptm@becamex.com.vn>
2024-01-27 15:17:14 +0700

    Add existing project files to Git

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const compression = require('compression')
+const express = require('express')
+const { default: helmet } = require('helmet')
+const morgan = require('morgan')
+const app = express()
+
+//init middlewares
+app.use(morgan('dev'))
+app.use(helmet())
+app.use(compression())
+
+module.exports = app
